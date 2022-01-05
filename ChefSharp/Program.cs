5d2d13feaa1d82e8168e64c6ba8024ec9d7d6348
James <jameshaller27@gmail.com>
2022-01-04 23:21:26 -0800

    Init commit

---
+++ ChefSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChefSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
